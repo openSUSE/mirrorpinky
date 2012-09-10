79977d62d4616b7851fd3e2ac7f0e0418a8ffa52
Marcus Rückert <darix@nordisch.org>
2012-09-10 18:04:13 +0200

    added validations and mass assignment whitelists

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,9 +1,42 @@
 require 'tzinfo'
+require 'email_validator'
+
 class Server < ActiveRecord::Base
   set_table_name 'server'
   has_one :country, :primary_key => :country, :foreign_key => :code
   has_one :region,  :primary_key => :region,  :foreign_key => :code
   has_many :files,  :class_name => 'MirrorFile', :finder_sql => proc { "SELECT * FROM filearr where #{id} = any(mirrors)" }
+  validates :other_countries, :format => { :with => /\A([a-z0-9]{2}(,[a-z0-9]{2})*)?\Z/ }
+  validates :baseurl,         :format => { :with => URI::regexp(%w(ftp http https)) }
+  validates :baseurl_ftp,     :format => { :with => URI::regexp(%w(ftp))        }, :allow_blank => true
+  validates :baseurl_rsync,   :format => { :with => URI::regexp(%w(rsync))      }, :allow_blank => true
+  validates :operator_url,    :format => { :with => URI::regexp(%w(http https)) }, :allow_blank => true
+  validates :asn,             :numericality => { :only_integer => true }
+  validates :score,           :numericality => { :only_integer => true }
+  validates :identifier,      :presence => true, :uniqueness => true
+  validates_with ::AdminEmailValidator
+
+  attr_accessible :admin, :admin_email, :as_only, :asn, :baseurl, :baseurl_ftp,
+    :baseurl_rsync, :comment, :country, :country_only, :enabled, :file_maxsize,
+    :id, :identifier, :last_scan, :lat, :lng, :operator_name, :operator_url,
+    :other_countries, :prefix, :prefix_only, :public_notes, :region,
+    :region_only, :scan_fpm, :score, :status_baseurl
+
+# TODO: check if we need validators for those
+#   status_baseurl: true
+#   lng: 9.933
+#   region_only: false
+#   file_maxsize: 0
+#   identifier: ftp4.gwdg.de
+#   enabled: true
+#   score: 120
+#   scan_fpm: 40180
+#   public_notes: ""
+#   as_only: false
+#   region: eu
+#   country: de
+#   prefix: 134.76.0.0/16
+#   comment: |-
 
   def starcount
     case score
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+require 'email_validator'
 class User < ActiveRecord::Base
   include Clearance::User
+  has_and_belongs_to_many :groups
+  has_many :servers, :through => :groups
+  validates_with ::EmailValidator
+  validates :email, :uniqueness => true
+  attr_accessible :email
 end
