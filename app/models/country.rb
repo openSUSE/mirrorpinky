e91f3a46944a5e54d2ad13594dcd7b2e8356e3c2
Marcus Rückert <mrueckert@suse.de>
2013-10-11 11:46:38 +0200

    - set primary key

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,5 @@
 class Country < ActiveRecord::Base
   self.table_name = 'country'
+  self.primary_key = 'code'
   has_many :servers, :primary_key => :code, :foreign_key => :country
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,5 @@
 class Region < ActiveRecord::Base
   self.table_name = 'region'
+  self.primary_key = 'code'
   has_many :servers, :primary_key => :code, :foreign_key => :region
 end
