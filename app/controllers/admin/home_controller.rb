32ffe8b5c876ba82bc2da72be49ebb77e56cce32
Marcus Rückert <mrueckert@suse.de>
2014-10-22 12:21:03 +0200

    - show also users in the overview page

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomeController < ApplicationController
   def index
      authorize! :admin, :dashboard
-     @groups = current_user.groups.includes(:servers).order('server.identifier')
+     @groups = current_user.groups.includes(:servers).includes(:users).order('server.identifier')
      @group_requests = current_user.group_requests
   end
 end
